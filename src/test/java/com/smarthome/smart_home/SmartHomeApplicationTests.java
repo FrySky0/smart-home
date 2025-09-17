00abc01a09962c19f2d9663bfb0a1b222654f1c5
FrySky0 <frysky0@mail.ru>
2025-09-18 01:26:42 +0300

    Initial commit

---
+++ src/test/java/com/smarthome/smart_home/SmartHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthome.smart_home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
